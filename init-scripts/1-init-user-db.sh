856d44fdcbe8656d62b6bd8dd37e6efe1d34355a
SleepingNext <veroskevin8@gmail.com>
2020-07-16 10:57:11 +0700

    Delete init-scripts

--- init-scripts/1-init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER sleepingnext;
-	CREATE DATABASE sleepingnext;
-	ALTER USER sleepingnext PASSWORD 'kevin99123';
-	GRANT ALL PRIVILEGES ON DATABASE sleepingnext TO sleepingnext;
-EOSQL
